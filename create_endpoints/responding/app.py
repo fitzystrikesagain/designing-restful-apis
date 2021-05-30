34e918750e666a933a309e6f12662d78c79fd68d
Fitzy <fitzystrikesagain@gmail.com>
2021-05-30 05:57:13 -0500

    finish "Responding to different kinds of requests"

--- create_endpoints/responding/app.py
+++ create_endpoints/responding/app.py
@@ -1,44 +1,51 @@
 from flask import Flask, request
+
 app = Flask(__name__)
+
+
 # Create the appropriate app.route functions, test and see if they work, and paste your URIs in the boxes below.
 
-#Make an app.route() decorator here
-
-def puppiesFunction():
+# Make an app.route() decorator here
+@app.route("/puppies", methods=["GET", "POST"])
+def puppies_function():
     if request.method == 'GET':
-    #Call the method to Get all of the puppies
-
+        # Call the method to Get all of the puppies
+        return get_all_puppies()
 
     elif request.method == 'POST':
-#Call the method to make a new puppy
+        # Call the method to make a new puppy
+        return make_new_puppy()
 
 
-
-#Make another app.route() decorator here that takes in an integer id in the
-
-def puppiesFunctionId(id):
+# Make another app.route() decorator here that takes in an integer id in the
+@app.route("/puppies/<puppy_id>", methods=["GET", "PUT", "DELETE"])
+def puppies_functionId(puppy_id):
     if request.method == 'GET':
-    #Call the method to get a specific puppy based on their id
-
+        # Call the method to get a specific puppy based on their id
+        return get_puppy(puppy_id)
     if request.method == 'PUT':
-    #Call the method to update a puppy
-
+        # Call the method to update a puppy
+        return update_puppy(puppy_id)
     elif request.method == 'DELETE':
-#Call the method to remove a puppy
+        # Call the method to remove a puppy
+        return delete_puppy(puppy_id)
 
 
-
-def getAllPuppies():
+def get_all_puppies():
     return "Getting All the puppies!"
 
-def makeANewPuppy():
+
+def make_new_puppy():
     return "Creating A New Puppy!"
 
-def getPuppy(id):
-    return "Getting Puppy with id %s" % id
 
-def updatePuppy(id):
-    return "Updating a Puppy with id %s" % id
+def get_puppy(puppy_id):
+    return f"Getting Puppy with id {puppy_id}"
 
-def deletePuppy(id):
-    return "Removing Puppy with id %s" % id
+
+def update_puppy(puppy_id):
+    return f"Updating a Puppy with id {puppy_id}"
+
+
+def delete_puppy(puppy_id):
+    return f"Removing Puppy with id {puppy_id}"
